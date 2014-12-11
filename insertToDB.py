aaa2dbab2cbb26e416b9c23f8ffc60bfeb59b87b
Balachander Ramachandran <dragontransformer@gmail.com>
2014-12-11 17:04:23 -0600

    small changes

--- insertToDB.py
+++ insertToDB.py
@@ -5,17 +5,18 @@ import csv
 import os
 
 def get_data(filename):
-	data_list = []
-	with open('data/movie_data/'+filename,mode='rU') as datafile:
-		reader = csv.reader(datafile)
-   		data_item = {}
-   		headers = reader.next()
-   		for row in reader:
-   			# print  row
-   			data_item = {headers[i]:row[i] for i in xrange(len(headers))}
-   			data_item['year'] = re.sub('\.csv','',filename)
-   			data_list.append(data_item);
-   	return data_list
+    data_list = []
+    with open('data/'+filename,mode='rU') as datafile:
+        reader = csv.reader(datafile)
+        data_item = {}
+        headers = reader.next()
+        for row in reader:
+            # print  row
+            data_item = {headers[i]:row[i] for i in xrange(len(headers))}
+            if 'year' not in data_item:
+                data_item['year'] = re.sub('\.csv','',filename)
+            data_list.append(data_item);
+    return data_list
 
 def get_all_filenames():
 	file_names = []
@@ -53,25 +54,29 @@ def write_list_of_hash_to_file(input_list,filename):
 		for row in input_list:
 			fwriter.writerow(row.values())
 
-def insert_into_db(data_list):
+#def insert_into_db(data_list):
 
 
 
 if __name__ == "__main__":
-	files = get_all_filenames()
-	all_data = []
-	for f in files:
-		all_data.extend(get_data(f))
-	# print len(all_data)
-	final_data = clean_data(all_data)
-	# print len(final_data)
-	write_list_of_hash_to_file(final_data,'final_data')
-	insert_into_db(final_data)
-	#Need to get all actors and movies list and insert it.
-	contributors = []
-	for row in final_data:
-		contributors.extend(re.sub('\[|\]|\'','',row['Contributors']).split(', '))
-	contributor_set = set(contributors)
+    files = get_all_filenames()
+    all_data = []
+    for f in files:
+        all_data.extend(get_data('movie_data/' + f))
+    # print len(all_data)
+    final_data = clean_data(all_data)
+    # print len(final_data)
+    write_list_of_hash_to_file(final_data,'final_data')
+    award_data = get_data('awards.csv')
+    print award_data[0]
+    print len(award_data)
+    #insert_into_db(final_data)
+    #Need to get all actors and movies list and insert it.
+    contributors = []
+    for row in final_data:
+        contributors.extend(re.sub('\[|\]|\'','',row['Contributors']).split(', '))
+    contributor_set = set(contributors)
+    
 
 
 
